41068537bd49917d73310c58dd4d177133cc33b4
savioquixaba <76010162+savioquixaba@users.noreply.github.com>
2021-02-09 14:07:47 -0300

    Create Soma.js

---
+++ Soma.js
@@ -0,0 +1,13 @@
+const number1 = 3;
+const number2 = 2;
+const operator = '+';
+let result;
+
+if (operator === '+') {
+    result = number1 + number2;
+}
+else {
+    result = ERROR;
+}
+
+console.log(result);
